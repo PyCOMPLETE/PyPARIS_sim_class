df81e7a46f9283381e5be1d5b34550f840215f80
giadarol <giovanni.iadarola@cern.ch>
2019-02-08 15:40:31 +0100

    A couple of helper scripts

---
+++ t000_build_single_instance.py
@@ -0,0 +1,7 @@
+import Simulation as sim_mod
+
+N_proc_pretend = 4
+myid_pretend = 2
+
+sim = sim_mod.get_sim_instance(N_proc_pretend, myid_pretend)
+
---
+++ t001_run_single_core.py
@@ -0,0 +1,4 @@
+import Simulation as sim_mod
+
+ring = sim_mod.get_serial_CPUring()
+ring.run()
