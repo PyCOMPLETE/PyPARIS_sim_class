952b5069c7e374c7a1b50b8c54a50b9e2a63f1e4
kparasch <konstantinos.paraschou@cern.ch>
2019-03-13 19:08:34 +0100

    remove some running scripts

--- examples/octupoles/001_run_multiproc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-export PYTHONPATH=$PYTHONPATH:../../../
-
-# Run Parallel without MPI
-../../../PyPARIS/multiprocexec.py -n 3 sim_class=PyPARIS_sim_class.Simulation.Simulation
-
-
--- examples/octupoles/002_run_MPI.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-export PYTHONPATH=$PYTHONPATH:../../../
-
-# Run MPI
-mpiexec -n 4 ../../../PyPARIS/withmpi.py sim_class=PyPARIS_sim_class.Simulation.Simulation
