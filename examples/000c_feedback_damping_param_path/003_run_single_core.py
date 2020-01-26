0d90d8e7782891facf9f896fa0d626a1b7eb3758
giadarol <giovanni.iadarola@cern.ch>
2020-01-26 21:02:00 +0100

    It works

--- examples/000c_feedback_damping_param_path/003_run_single_core.py
+++ examples/000c_feedback_damping_param_path/003_run_single_core.py
@@ -2,6 +2,10 @@ import sys
 sys.path.append('../../../')
 
 import PyPARIS_sim_class.Simulation as sim_mod
+import PyPARIS.util as pu
 
-ring = sim_mod.get_serial_CPUring()
+sim_content = sim_mod.Simulation(
+    param_file='param_folder/Simulation_parameters.py')
+
+ring = pu.get_serial_CPUring(sim_content, init_sim_objects_auto=True)
 ring.run()
